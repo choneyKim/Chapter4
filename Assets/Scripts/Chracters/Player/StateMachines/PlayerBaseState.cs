d4ee5674c751b9206a22317d586b58d8c3f5e7a6
김치헌 <chk87a@gmail.com>
2024-02-21 01:48:51 +0900

    [Feat] PlayerStateMachine

---
+++ Assets/Scripts/Chracters/Player/StateMachines/PlayerBaseState.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerBaseState : IState
+{
+    protected PlayerStateMachine stateMachine;
+    protected readonly PlayerGroundData groundData;
+    public PlayerBaseState(PlayerStateMachine playerStateMachine)
+    {
+        stateMachine = playerStateMachine;
+        groundData = stateMachine.Player.Data.GroundData;
+    }
+    public virtual void Enter()
+    {
+        AddInputActionsCallbacks();
+    }
+
+    public virtual void Exit()
+    {
+        RemoveInputActionsCallbacks();
+    }
+
+    public virtual void HandleInput()
+    {
+        ReadMovementInput();
+    }
+
+
+    public virtual void PhysicsUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public virtual void Update()
+    {
+        Move();
+    }
+
+
+    protected virtual void AddInputActionsCallbacks() 
+    {
+
+    }
+    protected virtual void RemoveInputActionsCallbacks()
+    {
+
+    }
+    private void ReadMovementInput()
+    {
+       stateMachine.MovementInput = stateMachine.Player.Input.PlayerActions.Movement.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        Vector3 movementDirection = GectMovementDirection();
+        Rotate(movementDirection);
+        Move(movementDirection);
+    }
+
+
+    private Vector3 GectMovementDirection()
+    {
+        Vector3 foward = stateMachine.MainCameraTransform.forward;
+        Vector3 right = stateMachine.MainCameraTransform.right;
+        foward.y = 0;
+        right.y = 0;
+
+        foward.Normalize();
+        right.Normalize();
+
+        return foward*stateMachine.MovementInput.y + right*stateMachine.MovementInput.x;
+    }
+    private void Move(Vector3 movementDirection)
+    {
+        float movementSpeed = GetMovementSpeed();
+        stateMachine.Player.Controller.Move(
+            (movementDirection * movementSpeed) * Time.deltaTime
+            );
+    }
+
+    private float GetMovementSpeed()
+    {
+        float movementSpeed = stateMachine.MovementSpeed * stateMachine.MovementSpeedModifier;
+        return movementSpeed;
+    }
+
+    private void Rotate(Vector3 movementDirection)
+    {
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movementDirection);
+            stateMachine.Player.transform.rotation =  Quaternion.Slerp(stateMachine.Player.transform.rotation, targetRotation, stateMachine.RotationDamping*Time.deltaTime);
+        }
+    }
+
+    protected void StartAnimation(int animationHash)
+    {
+        stateMachine.Player.Animator.SetBool(animationHash, true);
+    }
+    protected void StopAnimation(int animationHash)
+    {
+        stateMachine.Player.Animator.SetBool(animationHash, false);
+    }
+
+
+
+}
