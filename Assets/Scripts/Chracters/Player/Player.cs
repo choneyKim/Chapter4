90b9bdb26369c26ba204341536c5a5f9a02c94e7
김치헌 <chk87a@gmail.com>
2024-02-21 17:17:50 +0900

    [Modify] Player.cs (포스리시버 추가)

--- Assets/Scripts/Chracters/Player/Player.cs
+++ Assets/Scripts/Chracters/Player/Player.cs
@@ -13,6 +13,7 @@ public class Player : MonoBehaviour
     public Animator Animator { get; private set; }
     public PlayerInput Input { get; private set; }
     public CharacterController Controller { get; private set; }
+    public ForceReceiver ForceReceiver { get; private set; }
 
     private PlayerStateMachine stateMachine;
 
@@ -24,6 +25,7 @@ public class Player : MonoBehaviour
         Animator = GetComponentInChildren<Animator>();
         Input = GetComponent<PlayerInput>();
         Controller = GetComponent<CharacterController>();
+        ForceReceiver = GetComponent<ForceReceiver>();
 
         stateMachine = new PlayerStateMachine(this);
 
